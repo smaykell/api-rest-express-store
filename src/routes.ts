4b515f077aa2609a09a566f9044f520bb3b2c517
Smaykell <smaykellguerrero@gmail.com>
2023-04-07 10:59:33 -0500

    ADD Operaciones detalle carrito

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { AuthController } from "./controller/AuthController";
 import { CategoriasController } from "./controller/CategoriasControllers";
+import { DetallesCarritoController } from "./controller/DetallesCarritoController";
 import { ProductosController } from "./controller/ProductosController";
 
 export const Routes = [
@@ -36,6 +37,34 @@ export const Routes = [
     route: "/productos",
     controller: ProductosController,
     action: "findAll",
+    protected: true,
+  },
+  {
+    method: "get",
+    route: "/detalles-carrito",
+    controller: DetallesCarritoController,
+    action: "findByUserId",
+    protected: true,
+  },
+  {
+    method: "post",
+    route: "/detalles-carrito",
+    controller: DetallesCarritoController,
+    action: "addDetalleCarrito",
+    protected: true,
+  },
+  {
+    method: "put",
+    route: "/detalles-carrito/:id",
+    controller: DetallesCarritoController,
+    action: "updateDetalleCarrito",
+    protected: true,
+  },
+  {
+    method: "delete",
+    route: "/detalles-carrito/:id",
+    controller: DetallesCarritoController,
+    action: "deleteDetalleCarrito",
     protected: true,
   },
 ];
